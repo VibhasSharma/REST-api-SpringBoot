f4e931597ae153831a8289c3934a72a00738d71b
Vibhas Sharma <vibhascr734@gmail.com>
2022-01-26 13:24:07 +0530

    created Table schema using @Entity and @Table annotations

--- src/main/java/com/example/StudentRestAPI/Student/Student.java
+++ src/main/java/com/example/StudentRestAPI/Student/Student.java
@@ -1,9 +1,24 @@
 package com.example.StudentRestAPI.Student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
     private Long id;
     private String name;
     private String email;
