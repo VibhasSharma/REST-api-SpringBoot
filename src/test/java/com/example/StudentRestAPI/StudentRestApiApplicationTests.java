338a39001275c0286c1c9f11675672bf25f8dec0
Vibhas Sharma <vibhascr734@gmail.com>
2024-02-13 19:28:53 +0530

    Update StudentRestApiApplicationTests.java

--- src/test/java/com/example/StudentRestAPI/StudentRestApiApplicationTests.java
+++ src/test/java/com/example/StudentRestAPI/StudentRestApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.StudentRestAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//test class
 @SpringBootTest
 class StudentRestApiApplicationTests {
 
